e6d8ac3439c8c755cdbbab96e431613915415968
EvanC <evancole82590@gmail.com>
2014-07-02 16:09:01 -0400

    Renamed the folders and projects names to KennelSoftware

---
+++ KennelSoftware/Animal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KennelSoftware
+{
+    class Animal
+    {
+        private string name;
+        private string breed;
+        private float age;
+
+        public Animal(string name, string breed, float age)
+        {
+            this.name = name;
+            this.breed = breed;
+            this.age = age;
+        }
+
+        public string getAnimalName()
+        {
+            return this.name;
+        }
+
+        public string getAnimalBreed()
+        {
+            return this.breed;
+        }
+
+        public float getAnimalAge()
+        {
+            return this.age;
+        }
+    }
+}
---
+++ KennelSoftware/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KennelSoftware
+{
+    class Customer
+    {
+        private string customer_fname, customer_lname;
+        private List<Animal> animal;
+
+        public Customer(string fname, string lname)
+        {
+            customer_fname = "";
+            customer_lname = "";
+            animal = new List<Animal>();
+        }
+
+        public string getCustomerFirstName()
+        {
+            return customer_fname;
+        }
+
+        public string getCustomerLastName()
+        {
+            return customer_lname;
+        }
+    }
+}
